8af73efcaa775f93f379fa87dd509cb3706d9c99
uncle13th <570333293@qq.com>
2018-06-20 23:53:14 +0800

    修改注册相关代码

--- app/Http/Controllers/System/RegisterController.php
+++ app/Http/Controllers/System/RegisterController.php
@@ -2,7 +2,7 @@
 
 namespace App\Http\Controllers\System;
 
-use App\User;
+use App\Http\Model\System\UserModel;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
@@ -63,7 +63,7 @@ class RegisterController extends Controller
      */
     protected function create(array $data)
     {
-        return User::create([
+        return UserModel::create([
             'name' => $data['name'],
             'email' => $data['email'],
             'password' => Hash::make($data['password']),
